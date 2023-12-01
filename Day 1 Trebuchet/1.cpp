0e498ee23666d23898fa39cc6ba73447cf4e4dfc
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-12-01 23:06:24 +0000

    Improved Day 1 part 1.

--- Day 1 Trebuchet/1.cpp
+++ Day 1 Trebuchet/1.cpp
@@ -1,24 +1,27 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <locale>
 #include <string>
 
 int main() {
-  const char UNINITIALISED = '\0';
-  std::ifstream input(
-      "C:\\Users\\kevro\\Programming\\Advent Of Code\\AOC2023\\Day 1 "
-      "Trebuchet\\input.txt");
+  const char DEFAULT_CHAR = '\0';
+  std::filesystem::path filePath =
+      "C:/Users/kevro/Programming/Advent Of Code/AOC2023/Day 1 "
+      "Trebuchet/input.txt";
+
+  std::ifstream input(filePath);
   std::string line;
   int total = 0;
 
   if (input.is_open()) {
     while (input >> line) {
-      char first_number = UNINITIALISED;
-      char last_number = UNINITIALISED;
+      char first_number = DEFAULT_CHAR;
+      char last_number = DEFAULT_CHAR;
 
-      for (char& character : line) {
+      for (auto& character : line) {
         if (std::isdigit(character)) {
-          if (first_number == UNINITIALISED) {
+          if (first_number == DEFAULT_CHAR) {
             first_number = character;
             last_number = character;
           } else {
@@ -29,6 +32,14 @@ int main() {
 
       total += std::stoi(std::string(1, first_number) + last_number);
     }
+
+    input.close();
+  } else {
+    std::cerr << "Error opening file: " << filePath << std::endl;
+    return 1;
   }
+
   std::cout << total << std::endl;
+
+  return 0;
 }
