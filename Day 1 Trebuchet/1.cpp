d34941ccbbfb59953a45893a2bfbc8d98ffd8018
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-12-02 00:21:21 +0000

    Day 1 part 2 improvements.

--- Day 1 Trebuchet/1.cpp
+++ Day 1 Trebuchet/1.cpp
@@ -6,38 +6,38 @@
 
 int main() {
   const char DEFAULT_CHAR = '\0';
-  std::filesystem::path filePath =
+  const std::filesystem::path filePath =
       "C:/Users/kevro/Programming/Advent Of Code/AOC2023/Day 1 "
       "Trebuchet/input.txt";
-
   std::ifstream input(filePath);
-  std::string line;
-  int total = 0;
 
-  if (input.is_open()) {
-    while (input >> line) {
-      char first_number = DEFAULT_CHAR;
-      char last_number = DEFAULT_CHAR;
-
-      for (auto& character : line) {
-        if (std::isdigit(character)) {
-          if (first_number == DEFAULT_CHAR) {
-            first_number = character;
-            last_number = character;
-          } else {
-            last_number = character;
-          }
-        }
-      }
-
-      total += std::stoi(std::string(1, first_number) + last_number);
-    }
-
-    input.close();
-  } else {
+  if (!input.is_open()) {
     std::cerr << "Error opening file: " << filePath << std::endl;
     return 1;
   }
+
+  std::string line;
+  int total = 0;
+
+  while (input >> line) {
+    char first_number = DEFAULT_CHAR;
+    char last_number = DEFAULT_CHAR;
+
+    for (auto& character : line) {
+      if (std::isdigit(character)) {
+        if (first_number == DEFAULT_CHAR) {
+          first_number = character;
+          last_number = character;
+        } else {
+          last_number = character;
+        }
+      }
+    }
+
+    total += std::stoi(std::string(1, first_number) + last_number);
+  }
+
+  input.close();
 
   std::cout << total << std::endl;
 
--- Day 1 Trebuchet/2.cpp
+++ Day 1 Trebuchet/2.cpp
@@ -1,65 +1,72 @@
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <locale>
-#include <regex>
 #include <string>
+#include <vector>
 
 int main() {
   const char DEFAULT_CHAR = '\0';
-  std::filesystem::path filePath =
+  const std::filesystem::path filePath =
       "C:/Users/kevro/Programming/Advent Of Code/AOC2023/Day 1 "
       "Trebuchet/input.txt";
   std::ifstream input(filePath);
-  std::string line;
-  int total = 0;
-  std::vector<std::pair<std::regex, std::string>> substitutions = {
-      {std::regex("one"), "o1e"},    // o required in the case of 'twone',
-                                     // e required in the case of 'oneight'
-      {std::regex("two"), "t2"},     // t required in the case of 'eightwo'
-      {std::regex("three"), "t3e"},  // t required in the case of 'eighthree',
-                                     // e required in the case of 'threeight'
-      {std::regex("four"), "4"},
-      {std::regex("five"), "5e"},  // e required in the case of 'fiveight'
-      {std::regex("six"), "6"},
-      {std::regex("seven"), "7n"},  // n required in the case of 'sevenine'
-      {std::regex("eight"), "e8"},  // e required in the case of 'nineight'
-      {std::regex("nine"), "9"},
-  };
 
-  if (input.is_open()) {
-    while (input >> line) {
-      const std::string pre = line;
-      for (const auto& [pattern, substitution] : substitutions) {
-        line = std::regex_replace(line, pattern, substitution);
-      }
-
-      char first_number = DEFAULT_CHAR;
-      char last_number = DEFAULT_CHAR;
-
-      for (auto& character : line) {
-        if (std::isdigit(character)) {
-          if (first_number == DEFAULT_CHAR) {
-            first_number = character;
-            last_number = character;
-          } else {
-            last_number = character;
-          }
-        }
-      }
-      std::cout << pre << " " << line << " "
-                << std::stoi(std::string(1, first_number) + last_number)
-                << std::endl;
-      total += std::stoi(std::string(1, first_number) + last_number);
-    }
-
-    input.close();
-  } else {
+  if (!input.is_open()) {
     std::cerr << "Error opening file: " << filePath << std::endl;
     return 1;
   }
 
-  std::cout << total << std::endl;
+  std::string line;
+  int total = 0;
+
+  std::vector<std::pair<std::string, std::string>> substitutions = {
+      {"one", "o1e"},    // o required in the case of 'twone',
+                         // e required in the case of 'oneight'
+      {"two", "t2"},     // t required in the case of 'eightwo'
+      {"three", "t3e"},  // t required in the case of 'eighthree',
+                         // e required in the case of 'threeight'
+      {"four", "4"},
+      {"five", "5e"},  // e required in the case of 'fiveight'
+      {"six", "6"},
+      {"seven", "7n"},  // n required in the case of 'sevenine'
+      {"eight", "e8"},  // e required in the case of 'nineight'
+      {"nine", "9"},
+  };
+
+  while (input >> line) {
+    const std::string pre = line;
+    for (auto& [pattern, substitution] : substitutions) {
+      size_t pos = 0;
+      while ((pos = line.find(pattern, pos)) != std::string::npos) {
+        line.replace(pos, pattern.length(), substitution);
+        pos += substitution.length();
+      }
+    }
+
+    char first_number = DEFAULT_CHAR;
+    char last_number = DEFAULT_CHAR;
+
+    for (auto currentChar : line) {
+      if (std::isdigit(currentChar)) {
+        if (first_number == DEFAULT_CHAR) {
+          first_number = currentChar;
+          last_number = currentChar;
+        } else {
+          last_number = currentChar;
+        }
+      }
+    }
+
+    if (first_number != DEFAULT_CHAR) {
+      total += std::stoi(std::string(1, first_number) + last_number);
+    }
+  }
+
+  input.close();
+
+  std::cout << "Total: " << total << std::endl;
 
   return 0;
 }
